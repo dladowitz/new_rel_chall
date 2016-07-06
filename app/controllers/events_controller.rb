67f83e24672211b22a78d83afb0784ed7bb94e73
David Ladowitz <david@ladowitz.com>
2016-07-06 10:46:08 -0700

    finishing up

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
     @event = org.events.build event_params
 
     #TODO move to seperate method
-      #TODO need a way to look up the calling hostname and save into event. 
+      #TODO need a way to look up the calling hostname and save into event.
       #Can probably pull this out of the request object.
       #For the moment I'm just going to use the remote IP.
       @event.hostname = request.remote_ip
@@ -19,27 +19,16 @@ class EventsController < ApplicationController
   end
 
   def index
-    # binding.pry
     org = Org.find params[:org_id]
+    @events = find_events(params, org)
 
-    #TODO move this into another method as it doens't all belong in the index controller
-      #TODO create scopes for each of these on the model
-      number = event_params[:number] || 10 #defaulting to 10 events
-      reverse = event_params[:reverse] || nil
-      hostname = event_params[:hostname] || nil
-
-      @events = org.events.last(number)
-      @events = @events.reverse if reverse
-      @evetnts = @events.where(hostname: hostname) if hostname
-
-
+    # binding.pry
     respond_to do |format|
       if org
         format.json { @events }
         format.html { @events } #just using this for easy visualization. Will remove
       else
         format.json { "Doh! Can't find that Org."}
-        format.html { "Doh! Can't find that Org."}
       end
     end
   end
@@ -47,6 +36,14 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:string, :reverse, :hostname, :number)
+    params.require(:event).permit(:string)
+  end
+
+  def find_events(params, org)
+    events = org.events.last( || 10)
+    events = events.reverse if params[:reverse]
+    events = events.where(hostname: hostname) if params[:hostname]
+
+    return events
   end
 end
